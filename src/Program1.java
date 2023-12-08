b289d65649e716d1a4e2028d8039c88d435d70b7
SIVA KUMAR SELVAM <47846332+shivaguru7@users.noreply.github.com>
2023-12-08 10:54:54 +0530

    first commit

---
+++ src/Program1.java
@@ -0,0 +1,20 @@
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=10;
+		b=0;
+		try
+		{
+			c=a/b;
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+	}
+
+}
